b4d740f3a530e2b8f07e0cca01cd5efb2987caf2
Aymerick Toonba <godzilla.toonba@gmail.com>
2023-08-26 10:50:44 +0200

    changed loading effect on summary card

--- Components/Loader/Loader.jsx
+++ Components/Loader/Loader.jsx
@@ -1,6 +1,26 @@
 import styles from '../../styles/Loader.module.css'
 
 export default function Loader() {
+  const letters = '0123456789.,'
+
+  const hackerStyle = (e) => {
+    let iterations = 0
+    const interval = setInterval(() => {
+      e.target.innerText = e.target.innerText
+        .split('')
+        .map((letter, index) => {
+          if (index < iterations) {
+            return e.target.dataset.value[index]
+          }
+          return letters[Math.floor(Math.random() * 12)]
+        })
+        .join('')
+      if (iterations >= e.target.dataset.value.length) {
+        clearInterval(interval)
+      }
+      iterations += 1 / 3
+    }, 30)
+  }
   return (
     <div className={styles.loaderRing}>
       <div></div>
--- Components/SummaryCard/SummaryCard.jsx
+++ Components/SummaryCard/SummaryCard.jsx
@@ -1,13 +1,37 @@
+import { useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import styles from '../../styles/DataSummary.module.css'
 import Loader from '../Loader/Loader'
 
 export default function SummaryCard({ input, balance, title, showRange, isLoading, currency }) {
   const range = useSelector((state) => state.selectRangeValue)
+  const maRef = useRef(null)
+  const letters = '0123456789'
+
+  const hackerStyle = (ref) => {
+    let iterations = 0
+    const interval = setInterval(() => {
+      ref.current.innerText = (Math.random() * 1000).toFixed(2)
+      iterations += 1 / 3
+    }, 80)
+    return () => clearInterval(interval)
+  }
+
+  useEffect(() => {
+    if (isLoading) {
+      const cleanupHackerStyle = hackerStyle(maRef)
+      return cleanupHackerStyle // Nettoyer l'effet lorsque isLoading devient false
+    } else {
+      maRef.current.innerText = balance // Rétablir le contenu normal lorsque isLoading devient false
+    }
+  }, [isLoading, balance])
+
   return (
     <article className={styles.card}>
       {showRange === true ? <h2>{`${title} ${range} days`}</h2> : <h2>{title}</h2>}
-      <div className={styles.balance}>{input === '' ? null : isLoading === true ? <Loader /> : `${balance} ${currency === 'crypto' ? 'AVAX' : '$'}`}</div>
+      <div ref={maRef} className={styles.balance}>
+        {input === '' ? null : `${balance} ${currency === 'crypto' ? 'AVAX' : '$'}`}
+      </div>
     </article>
   )
 }
